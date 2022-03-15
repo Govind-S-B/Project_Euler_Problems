4d2d1ea2451d04d9e20cd46a3e7e7022c4523c11
Govind-S-B <b.s.dnivog@gmail.com>
2022-03-16 00:15:35 +0530

    problem 6

---
+++ 06 Sum square difference.py
@@ -0,0 +1,13 @@
+lim = 100
+
+sq_sum_diff = 0
+
+list_nums =  list(range(1,lim+1))
+for i in list_nums:
+    for j in list_nums:
+        if i!=None and j!=None and i!=j :
+            print(i,j)
+            sq_sum_diff += 2*i*j
+    list_nums[list_nums.index(i)] = None
+
+print(sq_sum_diff)
