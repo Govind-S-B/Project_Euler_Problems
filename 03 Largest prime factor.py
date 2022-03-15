877d583e577f0ea4e136d89dd4a6c53eab1f2470
Govind-S-B <b.s.dnivog@gmail.com>
2022-03-16 01:10:11 +0530

    fixed sieve implementation

--- 03 Largest prime factor.py
+++ 03 Largest prime factor.py
@@ -1,18 +1,38 @@
 n = 600851475143
 
-factors = []
-for i in range(1,int(n**(1/2))+1):
-    if n%i ==0:
-        factors.extend([i,n//i])
+def factor(num): # takes a int and returns set of factors
+    factors = set()
 
-i = 0
-while i<len(factors):
-    for j in factors:
-        if j%factors[i]==0  and j!=factors[i] :
-            factors.remove(j)
-    i+=1
+    for i in range(1,int(num**(1/2))+1): # list of factors (using sq root)
+        if num%i ==0:
+            factors.add(i)
+            factors.add(num//i)
 
-if len(factors) == 1:
-    factors.append(n)
+    return factors
 
-print(factors[-1])
+
+
+def prime_factor(num):
+    factor_list = list(factor(num))
+    factor_list.sort()
+    prime_factors = []
+
+    if len(factor_list) <= 2: # for input 1 and prime factors
+        prime_factors.append(factor_list[-1])
+    else:
+        factor_list.remove(1)
+        for i in factor_list: # sieve method
+            for j in factor_list:
+                if i!=None and j!=None and i!=j and j%i==0 :
+                    factor_list[factor_list.index(j)] = None
+
+    for i in factor_list:
+        if i!=None:
+            prime_factors.append(i)
+
+
+
+
+    return prime_factors
+
+print(prime_factor(n)[-1])
